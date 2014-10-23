c03cdf745deb9fd49ee69794992dbe872ec52dd9
Peter <monroepe@gmail.com>
2014-10-23 18:59:45 -0400

    Added devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
